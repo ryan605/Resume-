8f2dbd1a7ff77eb19f32a129bce4052661abcfb2
ryan605 <langatryan26@gmail.com>
2025-03-31 14:34:27 +0300

    header and into page added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { IntroSectionComponent } from './intro-section/intro-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, IntroSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
