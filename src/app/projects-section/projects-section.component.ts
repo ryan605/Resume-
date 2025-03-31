57fc4360271cd85dfeace61946cd6c2963382e8b
ryan605 <langatryan26@gmail.com>
2025-03-31 13:19:15 +0300

    added tailwind css and created custom components

---
+++ src/app/projects-section/projects-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects-section.component.html',
+  styleUrl: './projects-section.component.css'
+})
+export class ProjectsSectionComponent {
+
+}
